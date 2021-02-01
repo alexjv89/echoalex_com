0e687f3ed2c43cee561b42837749867a99696815
Alex <alexjv89@gmail.com>
2021-02-02 00:21:42 +0530

    image support

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,40 @@
 // const markdownItAnchor = require("markdown-it-anchor");
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy(".htaccess");
+  eleventyConfig.addPassthroughCopy("files");
+  eleventyConfig.addTransform("wiki-links", function (content, outputPath) {
+    if (outputPath && outputPath.endsWith(".html")) {
+      // We remove outer brackets from links
+      let output = content.replace(/(\[+(\<a(.*?)\<\/a\>)\]+)/g, "$2");
+      return output;
+    }
+    return content;
+  }); 
+  let markdownIt = require("markdown-it");
+  let markdownItReplaceLink = require("markdown-it-replace-link");
+  let markdownItOptions = {
+    html: true,
+    replaceLink: function (link, env) {
+      const isRelativePattern = /^(?!http|\/).*/;
+      const lastSegmentPattern = /[^\/]+(?=\/$|$)/i;
+      const isRelative = isRelativePattern.test(link);
+
+      if (isRelative) {
+        const hasLastSegment = lastSegmentPattern.exec(env.page.url);
+        // If it's nested, replace the last segment
+        if (hasLastSegment && env.page.url) {
+          return env.page.url.replace(lastSegmentPattern, link);
+        }
+        // If it's at root, just add the beginning slash
+        return env.page.url + link;
+      }
+
+      return link;
+    },
+  };
+  let markdownLib = markdownIt(markdownItOptions).use(markdownItReplaceLink);
+  eleventyConfig.setLibrary("md", markdownLib);
   // eleventyConfig.addPlugin(pluginRss);
   // eleventyConfig.addPlugin(pluginSyntaxHighlight);
   // eleventyConfig.addPlugin(pluginNavigation);
